83441b6d0460a60f2a1488de99c571edee7aff39
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-07 19:19:44 +0100

    tablero

--- ajedrez.py
+++ ajedrez.py
@@ -1,3 +1,6 @@
+from typing import BinaryIO
+
+
 f = open("ajedrez.txt", "w")
 """
 White pieces:		
@@ -8,10 +11,35 @@ Bishop = alfil	♗	&#x2657;
 Knight = caballo ♘	&#x2658;
 Pawn = peón	♙	&#x2659;
 Black pieces:		
-King	♚	&#x265a;
-Queen	♛	&#x265b;
-Rook	♜	&#x265c;
-Bishop	♝	&#x265d;
-Knight	♞	&#x265e;
-Pawn	♟	&#x265f;
+King = b_K	♚	&#x265a;
+Queen = b_Q	♛	&#x265b;
+Rook= b_R	♜	&#x265c;
+Bishop= b_B	♝	&#x265d;
+Knight= b_K	♞	&#x265e;
+Pawn= b_P	♟	&#x265f;
 """
+board = []
+black = ['b_R', 'b_K', 'b_B', 'b_K', 'b_Q', 'b_B1', 'b_K1', 'b_R1']
+white = ['w_R', 'w_K', 'w_B', 'w_K', 'w_Q', 'w_B1', 'w_K1', 'w_R1']
+black_pawn = ['b_P', 'b_P', 'b_P', 'b_P', 'b_P', 'b_P', 'b_P', 'b_P']
+white_pawn = ['w_P', 'w_P', 'w_P', 'w_P', 'w_P', 'w_P', 'w_P', 'w_P']
+
+def boardcreation():
+    
+    for i in range(0,8):
+        if i == 0:
+            board.append(black)
+        if i == 1:
+            board.append(black_pawn)
+        if i == 6:
+            board.append(white_pawn)
+        if i == 7:
+            board.append(white)
+        else:
+            board.append(['a'] * 8)
+            
+                
+    
+boardcreation()
+for x in board:
+    print(" ".join(x))
