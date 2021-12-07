f5215ca15f776bd8b5d83fa6e101141658ff2e82
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-07 19:56:13 +0100

    crear tablero

--- ajedrez.py
+++ ajedrez.py
@@ -1,5 +1,3 @@
-f = open("ajedrez.txt", "w")
-
 board = []
 black = ['b_R', 'b_K', 'b_B', 'b_K', 'b_Q', 'b_B1', 'b_K1', 'b_R1']
 white = ['w_R', 'w_K', 'w_B', 'w_K', 'w_Q', 'w_B1', 'w_K1', 'w_R1']
@@ -11,17 +9,28 @@ def boardcreation():
     for i in range(0,8):
         if i == 0:
             board.append(black)
-        if i == 1:
+        elif i == 1:
             board.append(black_pawn)
-        if i == 6:
+        elif i == 6:
             board.append(white_pawn)
-        if i == 7:
+        elif i == 7:
             board.append(white)
         else:
-            board.append(['a'] * 8)
+            board.append(['_'] * 8)
             
                 
     
 boardcreation()
 for x in board:
-    print(" ".join(x))
+    print("  ".join(x))
+
+f = open("ajedrez.txt", "w")
+for x in range(0,8):
+    for y in range(0,8):
+        
+        if y == 7: 
+            f.write(str(board[x][y] + '\n'))
+        else:
+            f.write(str(board[x][y]) + '\t')
+    
+f.close()
