188ecb46a977f9be49d5a74e76fd94ec654630f6
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-07 18:06:20 +0100

    fichero y piezas

---
+++ ajedrez.py
@@ -0,0 +1,17 @@
+f = open("ajedrez.txt", "w")
+"""
+White pieces:		
+King	♔	&#x2654;
+Queen	♕	&#x2655;
+Rook = torre	♖	&#x2656;
+Bishop = alfil	♗	&#x2657;
+Knight = caballo ♘	&#x2658;
+Pawn = peón	♙	&#x2659;
+Black pieces:		
+King	♚	&#x265a;
+Queen	♛	&#x265b;
+Rook	♜	&#x265c;
+Bishop	♝	&#x265d;
+Knight	♞	&#x265e;
+Pawn	♟	&#x265f;
+"""
